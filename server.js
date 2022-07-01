5cd1553c97123cd646a487a35688ccbc70a590f0
adarsh kumar khosla <khoslaadarsh@gmail.com>
2022-07-02 04:19:08 +0530

    First Commit, Video calling implemented

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const moment = require('moment');
-const {userJoin, GetCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
+const {userJoin, GetCurrentUser, userLeave, getRoomUsers} = require('./backend/utils/users');
 const e = require('express');
 const server = require('http').createServer(app);
-const port = process.env.PORT || 50000
+const port = process.env.PORT || 5000
 
 const io = require('socket.io')(server);
-const publicPath = path.join(__dirname, '/../public');
+const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
